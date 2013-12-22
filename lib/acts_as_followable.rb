98a0f5702e43989dc4af7f554ff76a6c69306662
Qi Li <cloudbsd@gmail.com>
2013-12-22 23:32:45 +0800

    initial import acts_as_followable plugin.

---
+++ lib/acts_as_followable.rb
@@ -0,0 +1,2 @@
+module ActsAsFollowable
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'ef4ae1fd8989211d3a961f8a0fb69dc471cc38a4bcef96795c9b07f44b4ffd2b0614003dd13881fc0ab8cd08bf855a48cfcbe8256ab39e5f2a5e797c7a5ff319'
